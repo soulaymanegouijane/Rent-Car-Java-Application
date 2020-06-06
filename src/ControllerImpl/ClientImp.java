d2f7293183c8aaaf248fb9eb3a4f391dc58724d8
You-97 <youssefelgourari97@gmail.com>
2020-06-06 23:22:09 +0100

    edit

--- src/ControllerImpl/ClientImp.java
+++ src/ControllerImpl/ClientImp.java
@@ -11,6 +11,7 @@ import AbstactClasses.Abst;
 import Entities.Client;
 import Entities.Reservation;
 import Interfaces.ClientInter;
+import Test.H;
 import javafx.collections.ObservableList;
 
 public class ClientImp extends Abst implements ClientInter{
@@ -64,46 +65,36 @@ public class ClientImp extends Abst implements ClientInter{
 
 	@Override
 	public Client edit(Client arg) {
-		String sql = "UPDATE Client SET nom=?, prenom=?, adress=?, telephone=?, email=?, age=?, civilite=?, date_naissance=?,"
-				+ "lieu_naissance=?,n_permis=?,delevre_le=? , validitePermis=?, delevre_a=?,type_identifiant=? , num_carte=?, validitePI=?  where idClient=?";
+		String sql = "UPDATE Client SET nom = ?, prenom=?, adress=?, telephone=?, email=?, age=?, civilite=?, date_naissance=?," + 
+				"lieu_naissance=?,n_permis=?,delevre_le=? , validitePermis=?, delevre_a=?,type_identifiant=? , num_carte=?, validitePI=?,code_postale=?,ville=?,pays=?,nationalite=?  where idClient=?";
 		Connection con = Abst.getConnection();
+		Client clt =null;
 		try {
-			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1,arg.getNom());
-			ps.setString(2,arg.getPrenom());
-			ps.setString(3,arg.getAdress());
-			ps.setString(4,arg.getTelephone());
-			ps.setString(5,arg.getEmail());
-			ps.setInt(5, arg.getAge());
-			ps.setString(5,arg.getCivilite());
-			//ps.setDate(1, arg.getDate_naissance());
-			ps.setString(5,arg.getLieu_naissance());
-			ps.setString(5,arg.getN_permis());
-			//ps.setDate(1, arg.getDelevre_le());
-			//ps.setDate(1, arg.getValiditePermis());
-			ps.setString(5,arg.getDelevre_a());
-			ps.setString(5,arg.getCarte_identifiant());
-			ps.setString(5,arg.getNum_carte());
-			//ps.setDate(1, arg.getValiditePI());
-			ps.setString(6, arg.getIdClient());
-			ps.executeUpdate();
+			PreparedStatement preparedStatement = con.prepareStatement(sql);
+			preparedStatement.setString(1,arg.getNom());
+			preparedStatement.setString(2,arg.getPrenom());
+			preparedStatement.setString(3,arg.getAdress());
+			preparedStatement.setString(4,arg.getTelephone());
+			preparedStatement.setString(5,arg.getEmail());
+			preparedStatement.setInt(6,arg.getAge());
+			preparedStatement.setString(7,arg.getCivilite());
+			preparedStatement.setString(8,arg.getDate_naissance());
+			preparedStatement.setString(9,arg.getLieu_naissance());
+			preparedStatement.setString(10,arg.getN_permis());
+			preparedStatement.setString(11,arg.getDelevre_le());
+			preparedStatement.setString(12,arg.getValiditePermis());
+			preparedStatement.setString(13,arg.getDelevre_a());
+			preparedStatement.setString(14,arg.getCarte_identifiant());
+			preparedStatement.setString(15,arg.getNum_carte());
+			preparedStatement.setString(16,arg.getValiditePI());
+			preparedStatement.setString(17, arg.getCode_postale());
+			preparedStatement.setString(18, arg.getVille());
+			preparedStatement.setString(19, arg.getPays());
+			preparedStatement.setString(20, arg.getNationalite());
+			preparedStatement.setString(21, arg.getIdClient());
+			preparedStatement.executeUpdate();
 			arg.setIdClient(arg.getIdClient());
-			arg.setAdress(arg.getAdress());
-			arg.setNom(arg.getNom());
-			arg.setPrenom(arg.getPrenom());
-			arg.setTelephone(arg.getTelephone());
-			arg.setEmail(arg.getEmail());
-			arg.setIdClient(arg.getIdClient());
-			arg.setAge(arg.getAge());
-			arg.setCivilite(arg.getCivilite());
-			arg.setDate_naissance(arg.getDate_naissance());
-			arg.setLieu_naissance(arg.getLieu_naissance());
-			arg.setN_permis(arg.getN_permis());
-			arg.setDelevre_a(arg.getDelevre_a());
-			arg.setCarte_identifiant(arg.getCarte_identifiant());
-			arg.setNum_carte(arg.getNum_carte());
-			arg.setValiditePermis(arg.getValiditePermis());
-			arg.setValiditePI(arg.getValiditePI());
+			clt = getById(arg.getIdClient());
 			
 			
 		} catch (SQLException e) {
@@ -117,7 +108,7 @@ public class ClientImp extends Abst implements ClientInter{
 			}
 		}
 		
-		return arg;
+		return clt;
 	}
 
 	@Override
--- src/InterfaceDetails/DetailClient.java
+++ src/InterfaceDetails/DetailClient.java
@@ -4,10 +4,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
+
+import Entities.Client;
+import Test.H;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class DetailClient implements Initializable{
@@ -74,17 +80,94 @@ public class DetailClient implements Initializable{
     @FXML
     public JFXButton editClientBtn;//boutton Modifier
     
+    String prenom = null;
+    String nom = null;
+    String nationalite = null;
+    String dateNaissance = null;
+    String lieuNaissance = null;
+    String cinClient = null;
+    String numPermis = null;
+    String lieuDelivre = null;
+    String dateDelivre = null;
+    String dateExpire = null;
+    String adresse = null;
+    String codePostal = null;
+    String pays = null;
+    String telephone = null;
+    String email = null;
+    String ville = null;
+    String genre = null;
+    String typeIdentifiant = null;
     
-    
-    @FXML
-    public void closeButtonAction(){
-    Stage stage =(Stage) closeButton.getScene().getWindow();
-    stage.close();
-}
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		String str = prenomTextField.getText();		
+		
 	}
+	
+	@FXML
+    public void closeButtonAction(){
+	    Stage stage =(Stage) closeButton.getScene().getWindow();
+	    stage.close();
+    }
+	
+	@FXML
+    void modifierClient(ActionEvent event) {
+		System.out.println("--------------->  * modification vehicule");
+		prenom = prenomTextField.getText();
+        nom = nomTextField.getText();
+        nationalite = nationaliteTextField.getText();
+        dateNaissance = ((TextField)dateNaissanceDatePicker.getEditor()).getText();
+        lieuNaissance = lieuNaissanceTextField.getText();
+        cinClient = cinTextField.getText();
+        numPermis = numPermisTextField.getText();
+        lieuDelivre = lieuDelivreTextField.getText();
+        dateDelivre = ((TextField)dateDelivreDatePicker.getEditor()).getText();
+        dateExpire = ((TextField)dateExpireDatePicker.getEditor()).getText();
+        adresse = adresseTextField.getText();
+        codePostal = codePostalTextField.getText();
+        pays = paysTextField.getText();
+        telephone = telephoneTextField.getText();
+        email = emailTextField.getText();
+        ville = villeTextField.getText();
+//        genre = comboGender.getValue();
+//        typeIdentifiant = comboIdType.getValue();
+        Client client = new Client();
+        
+        if(testEmpty()){
+        	Alert alert = new Alert(AlertType.ERROR);
+        	alert.setTitle("Alert d'erreur");
+        	alert.setHeaderText("can not add Client");
+        	alert.setContentText("Client n'est pas Modifier !!");
+        	alert.showAndWait();
+        }else{
+        	client.setAdress(adresse);
+        	client.setNom(nom);
+        	client.setPrenom(prenom);
+        	client.setNationalite(nationalite);
+        	client.setDate_naissance(dateNaissance);
+        	client.setLieu_naissance(lieuNaissance);
+        	client.setIdClient(cinClient);
+        	client.setN_permis(numPermis);
+        	client.setDelevre_le(lieuDelivre);
+        	client.setDelevre_a(dateDelivre);
+        	client.setPays(pays);
+        	client.setTelephone(telephone);
+        	client.setEmail(email);
+        	client.setCode_postale(codePostal);
+        	client.setCivilite(genre);
+        	client.setCarte_identifiant(typeIdentifiant);
+        	client.setVille(ville);
+        	
+        	H.client.edit(client);
+        }
+    }
+	
+	public boolean testEmpty(){
+        if(prenom.isEmpty() || nom.isEmpty() || nationalite.isEmpty() || dateNaissance == null || lieuNaissance.isEmpty()
+                || cinClient.isEmpty() || numPermis.isEmpty() || lieuDelivre.isEmpty() || dateDelivre == null
+                || dateExpire == null || adresse.isEmpty() || codePostal.isEmpty() || pays.isEmpty() || telephone.isEmpty()
+                || email.isEmpty()) return true;
+        return false;
+    }
 
 }
